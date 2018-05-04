a768668d76d2ed9fc7f9c4ed1ea238141492c02a
Luis <luisfrb56@gmail.com>
2018-05-04 09:37:58 +0200

    Clase creada y terminada

--- src/org/luis/pluginremoto/PluginRemotoCOD.java
+++ src/org/luis/pluginremoto/PluginRemotoCOD.java
@@ -7,11 +7,14 @@ package org.luis.pluginremoto;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 import javax.swing.JOptionPane;
 import org.openide.awt.ActionID;
 import org.openide.awt.ActionReference;
 import org.openide.awt.ActionRegistration;
 import org.openide.util.NbBundle.Messages;
+import org.kohsuke.github.GHCreateRepositoryBuilder;
+import org.kohsuke.github.GitHub;
 
 @ActionID(
         category = "File",
@@ -24,9 +27,23 @@ import org.openide.util.NbBundle.Messages;
 @ActionReference(path = "Menu/Versioning", position = 0)
 @Messages("CTL_PluginRemotoCOD=PluginRemoto")
 public final class PluginRemotoCOD implements ActionListener {
-
+//Aqui es donde vamos a mandar el repositorio remoto
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        //Primero pedimos un nombre para el repositorio que despues vamos a usar.
+        String nombre=JOptionPane.showInputDialog("Inserte el nombre del repositorio:");
+        try{
+            //Conectamos con github atraves .connect
+            GitHub github=GitHub.connect();
+            //Creamos el repositorio
+            GHCreateRepositoryBuilder builder;
+            //Insertamos el nombre que deseamos
+            builder=github.createRepository(nombre);
+            //Fin de la creacion.
+            builder.create();
+        }catch(IOException ex){
+            //Si da algun error al subirlo o con el usuario saltara esta excepcion.
+            System.out.println("Error:"+ex);
+        }
     }
 }
